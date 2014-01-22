9dfaee6679b08325675a92352df0030d1a7a4c48
inchonjjin <inchonjin@gmail.com>
2014-01-22 14:26:00 +0900

    Git Email test

--- HelloWithStructureMap/Program.cs
+++ HelloWithStructureMap/Program.cs
@@ -13,11 +13,7 @@ namespace HelloWithStructureMap
             IAppEngine appEngine = container.GetInstance<IAppEngine>();
             appEngine.Run();
 
-<<<<<<< HEAD
-            //test                 
-=======
-            //test test
->>>>>>> f808bd5bb47f1accc07b1b9225f2338764586b3f
+            //GitHub mailing test
             Console.ReadKey();
         }
 
